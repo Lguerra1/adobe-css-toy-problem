f62bbae343ee944a2a25d052a33e23bb46df64a2
tmat06 <timmy.a.matthews@gmail.com>
2018-07-22 22:46:34 -0600

    base

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div
+        className="App"
+        style={{
+          width: "100%",
+          height: "100vh",
+          backgroundColor: "grey"
+        }}
+      >
+        <div>Our Canvas</div>
+      </div>
+    );
   }
 }
 
